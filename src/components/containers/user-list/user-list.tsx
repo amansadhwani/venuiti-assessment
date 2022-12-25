9cfc27597b5ee0b3364e615acb6e454acb0455c1
Amankumar Sadhwani <amankumar_sadhwani@epam.com>
2022-12-25 17:06:51 +0530

    enhanced table

--- src/components/containers/user-list/user-list.tsx
+++ src/components/containers/user-list/user-list.tsx
@@ -10,57 +10,64 @@ export const UserList: FC<TUserProps> = ({ removeUser, sortName }) => {
   return (
     <>
       <div id="infinite-main-item-div" className="infinite-main-item-div">
-        <table data-testid="user-list-table">
-          <tbody>
-            <tr>
-              <th>User ID</th>
-              <th>
-                Name
-                <span
-                  className="fa fa-sort name"
-                  onClick={() =>
-                    sortName(
-                      nameSort === SORT_TYPE.AES
-                        ? SORT_TYPE.DEC
-                        : SORT_TYPE.AES,
-                      "name"
-                    )
-                  }
-                ></span>
-              </th>
-              <th>
-                Email
-                <span
-                  className="fa fa-sort email"
-                  onClick={() =>
-                    sortName(
-                      emailSort === SORT_TYPE.AES
-                        ? SORT_TYPE.DEC
-                        : SORT_TYPE.AES,
-                      "email"
-                    )
-                  }
-                ></span>
-              </th>
-              <th>Post</th>
-              <th>Delete User</th>
-            </tr>
-            {users?.slice(0, currentData).map((item: TUsers) => (
-              <tr key={item.id} data-testid={`user-list-data-${item.id}`}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.body}</td>
-                <td className="remove-btn">
+        <div className="container table-responsive py-3">
+          <table
+            className="table table-bordered table-hover"
+            data-testid="user-list-table"
+          >
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">
+                  Name
                   <span
-                    onClick={() => removeUser(item.id)}
-                    className="fa fa-trash"
+                    className="fa fa-sort name"
+                    onClick={() =>
+                      sortName(
+                        nameSort === SORT_TYPE.AES
+                          ? SORT_TYPE.DEC
+                          : SORT_TYPE.AES,
+                        "name"
+                      )
+                    }
                   ></span>
-                </td>
+                </th>
+                <th scope="col">
+                  Email
+                  <span
+                    className="fa fa-sort email"
+                    onClick={() =>
+                      sortName(
+                        emailSort === SORT_TYPE.AES
+                          ? SORT_TYPE.DEC
+                          : SORT_TYPE.AES,
+                        "email"
+                      )
+                    }
+                  ></span>
+                </th>
+                <th scope="col">Post</th>
+                <th scope="col">Delete User</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users?.slice(0, currentData).map((item: TUsers) => (
+                <tr key={item.id} data-testid={`user-list-data-${item.id}`}>
+                  <th scope="row">{item.id}</th>
+                  <th>{item.name}</th>
+                  <th>{item.email}</th>
+                  <th>{item.body}</th>
+                  <th className="remove-btn">
+                    <span
+                      onClick={() => removeUser(item.id)}
+                      className="fa fa-trash"
+                    ></span>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
