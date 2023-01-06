0185190a10d1aa173f4b1dc4d3a64bd8b096a56e
Amankumar Sadhwani <Amankumar_Sadhwani@epam.com>
2023-01-06 12:42:46 -0500

    added loader

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,12 @@
+
+import "./loader.css";
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+    <div className="loder-crcil"></div>
+      <div className="text">Fetching  ...</div>
+    </div>
+  )
+}
+
+export default Loader;
